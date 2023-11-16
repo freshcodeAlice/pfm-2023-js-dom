f31fa752ad42f4cd06dc5504779dcdb2e34093c8
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-16 12:01:02 +0200

    creating elements

--- index.js
+++ index.js
@@ -22,15 +22,17 @@ Model
 
 */
 
-const articlesHTMLCollection = document.getElementsByTagName('article');
-
-const articlesNodeList = document.querySelectorAll('article');
+const elem = document.createElement('button');
+elem.textContent = 'Click me please'
 
 /*
-HTMLCollection - "жива" колекція.
-При видаленні (або створенні нових елементів) формує актуальний список посилань на елементи (при видаленні буде зі списку видаляти їх, при створенні додавати)
+Створення елементів:
 
-NodeList - статична колекція, один раз сформований список не зміниться, якщо елементи будуть видалені або додані
+1. Створити об'єкт потрібного елемента
+2. Вказати батьківському елементу, що цей стане його дитиною
+    Для цього потрібно мати доступ до елемента (батька), який вже є на сторінці
 
-Якщо потрібно оновити список елементів - можна перевикликати querySelectorAll і перестворити NodeList заново
 */
+
+const section = document.querySelector('section');
+section.append(elem)
