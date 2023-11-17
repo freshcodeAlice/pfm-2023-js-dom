74ee0ada72c9cb416861596a3a09813a29a4109b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-17 09:56:27 +0200

    task resolve

--- index.js
+++ index.js
@@ -32,11 +32,22 @@ Model
 
 */
 
-const element = document.createElement('p');  // створюється HTMLParagraphElement
 
-element.textContent = 'PARAGRAPH element';
-element.style.color = 'violet';
+/*
+Створити пусту сторінку (без елементів).
+Динамічно створити секцію, всередині якої параграф і кнопка. В параграфі та кнопці довільний текст
+
+*/
 
 const body = document.body;
 
-body.append(element);
+const p = document.createElement('p');
+p.textContent = 'Довільний текст';
+
+const btn = document.createElement('button');
+btn.textContent = 'Клацни мене';
+
+const section = document.createElement('section');
+section.append(p, btn);
+
+body.append(section);
