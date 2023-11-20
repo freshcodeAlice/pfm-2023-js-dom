5a0b4f4862965958ff8607c7b1a4a7b816521140
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-20 11:27:50 +0200

    modalWindow example

--- index.js
+++ index.js
@@ -23,83 +23,43 @@
 */
 
 
-/*
-Створити структуру:
 
-body -> main -> section -> article
+const showBtn = document.querySelector('#show-modal');
+const closeBtn = document.querySelector('#close-modal');
 
-Задати елементам розміри (різні) і різні фонові кольори
-
-1. За натиснення на елемент-дитину (article) вивести його назву на консоль
-
-2. Навісити обробку подій на всі елементи, вивести currentTarget та target у кожному
-
-3. За допомогою керування подією, зупинити передачу події на main
-
-4. (навпаки) - зробити, щоб відпрацював handler на article і не відпрацювали всі інші
-*/
-
-//1
-const article = document.querySelector('article');
-const body = document.body;
-const main = document.querySelector('main');
-const section = document.querySelector('section');
-
-/*
-article.addEventListener('click', function (event) {
-    console.log(event.target)
-})
-*/
+const section = document.querySelector('.background');
+const modal = document.querySelector('.modal-window');
 
 
-//2
+function showModal() {
+   
+    section.style.display = 'flex';
+    modal.style.display = 'block';
+}
 
-/*
-function clickHandler(event) {
-    console.log('currentTarget:', event.currentTarget);
-    console.log('target', event.target);
+function closeModal() {
+    section.style.display = 'none';
+    modal.style.display = 'none';
 }
 
 
-article.addEventListener('click', clickHandler);
-section.addEventListener('click', clickHandler);
-main.addEventListener('click', clickHandler);
+showBtn.addEventListener('click', showModal);
+closeBtn.addEventListener('click', closeModal);
+section.addEventListener('click', closeHandler, true);
 
-body.addEventListener('click', clickHandler);
-
-*/
-
-
-//3 
 /*
-function clickHandler(event) {
-    if (event.currentTarget === main) {
-        event.stopPropagation();
+function closeHandler(event) {
+    if (event.target === event.currentTarget) {
+        closeModal();
     }
-    console.log('currentTarget:', event.currentTarget);
-    console.log('target', event.target);
 }
 
-
-article.addEventListener('click', clickHandler, true);
-section.addEventListener('click', clickHandler, true);
-main.addEventListener('click', clickHandler, true);
-
-body.addEventListener('click', clickHandler, true);
 */
 
-///4
 
-article.addEventListener('click', clickHandler);
-section.addEventListener('click', clickHandler);
-main.addEventListener('click', clickHandler);
-
-body.addEventListener('click', clickHandler);
-
-function clickHandler(event) {
-    if (event.currentTarget === event.target) {
+function closeHandler(event) {
+    if(event.target === event.currentTarget) {
         event.stopPropagation();
+        closeModal();
     }
-    console.log('currentTarget:', event.currentTarget);
-    console.log('target', event.target);
 }
