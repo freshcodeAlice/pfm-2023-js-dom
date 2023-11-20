fb4cb353bec0e838f5dfacb5182ba2e650bfe1b4
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-20 10:55:50 +0200

    event propagation tasks resolve

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-const body = document.body;
-
-const section = document.querySelector('section');
-const article = document.querySelector('article');
-const div = document.querySelector('#box');
-
-
-function handler(event) {
-    console.log('--------------')
-    console.log('currentTarget:', event.currentTarget);
-    console.log('target:', event.target);
-  
-  
-}
-
-body.addEventListener('click', handler);
-
-section.addEventListener('click', handler);
-
-article.addEventListener('click', handler);
-
-div.addEventListener('click', handler);
-
-
-/*
-function clicker(event) {
-    if(event.target === event.currentTarget) {
-    console.log('ви натиснули на секцію');
-    } else {
-        console.log('ви натиснули на дів');
-    }
-}
-
-
-section.addEventListener('click', clicker)
-*/
-
 
 /*
 Три фази події:
@@ -60,3 +23,83 @@ section.addEventListener('click', clicker)
 */
 
 
+/*
+Створити структуру:
+
+body -> main -> section -> article
+
+Задати елементам розміри (різні) і різні фонові кольори
+
+1. За натиснення на елемент-дитину (article) вивести його назву на консоль
+
+2. Навісити обробку подій на всі елементи, вивести currentTarget та target у кожному
+
+3. За допомогою керування подією, зупинити передачу події на main
+
+4. (навпаки) - зробити, щоб відпрацював handler на article і не відпрацювали всі інші
+*/
+
+//1
+const article = document.querySelector('article');
+const body = document.body;
+const main = document.querySelector('main');
+const section = document.querySelector('section');
+
+/*
+article.addEventListener('click', function (event) {
+    console.log(event.target)
+})
+*/
+
+
+//2
+
+/*
+function clickHandler(event) {
+    console.log('currentTarget:', event.currentTarget);
+    console.log('target', event.target);
+}
+
+
+article.addEventListener('click', clickHandler);
+section.addEventListener('click', clickHandler);
+main.addEventListener('click', clickHandler);
+
+body.addEventListener('click', clickHandler);
+
+*/
+
+
+//3 
+/*
+function clickHandler(event) {
+    if (event.currentTarget === main) {
+        event.stopPropagation();
+    }
+    console.log('currentTarget:', event.currentTarget);
+    console.log('target', event.target);
+}
+
+
+article.addEventListener('click', clickHandler, true);
+section.addEventListener('click', clickHandler, true);
+main.addEventListener('click', clickHandler, true);
+
+body.addEventListener('click', clickHandler, true);
+*/
+
+///4
+
+article.addEventListener('click', clickHandler);
+section.addEventListener('click', clickHandler);
+main.addEventListener('click', clickHandler);
+
+body.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+    if (event.currentTarget === event.target) {
+        event.stopPropagation();
+    }
+    console.log('currentTarget:', event.currentTarget);
+    console.log('target', event.target);
+}
