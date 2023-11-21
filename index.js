abede38a6ad0974cca35bb190e75461179d55923
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-21 11:10:28 +0200

    create Attribute

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-document.addEventListener('keydown', function(event) {
-    const STEP = 10;
-    const box = event.currentTarget.body.children.box;
-    const x = box.offsetLeft;
-    const y = box.offsetTop;
-    switch(event.code){
-        case 'ArrowUp': {
-            // перемістити елемент наверх
-            box.style.top = `${y - STEP}px`;
-            break
-        }
-        case 'ArrowDown': {
-            // перемістити елемент вниз
-            box.style.top = `${y + STEP}px`;
-            break;
-        }
-        case 'ArrowRight': {
-            // переміщаємось вправо
-            box.style.left = `${x + STEP}px`;
-            break;
-        }
-        case 'ArrowLeft': {
-            // переміщаємось вліво
-            box.style.left = `${x - STEP}px`;
-            break;
-        }
-    }
-})
+const addressURIimage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/800px-Shaqi_jrvej.jpg';
+
+const img = document.querySelector('.image');
+
+const attrSrc = document.createAttribute('src');
+img.setAttributeNode(attrSrc);
+
+attrSrc.value = addressURIimage;
+
+
+function changeURI(attrSrc) {
+    const anotherURI = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/1200px-Altja_j%C3%B5gi_Lahemaal.jpg';
+    attrSrc.value = anotherURI
+}
+
+
+
+//img.setAttribute('src', addressURIimage);
