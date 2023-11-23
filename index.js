82e1be380fa7d4a284eea4cf4819d171ea80b19c
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-23 10:38:21 +0200

    data + classlist

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const btns = document.querySelectorAll('button');
 
 btns.forEach(btn => {
     btn.addEventListener('click', function({target}){
-        target.style.backgroundColor = target.dataset.color;
+        target.classList.add(target.dataset.class);
     })
 })
