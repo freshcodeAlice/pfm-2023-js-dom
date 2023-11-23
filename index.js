7ca104f4aa3c2db73bdead3e4d3d73f75f1db806
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-23 09:25:47 +0200

    redCircle-greenSquare task resolve

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-const images = [
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/800px-Shaqi_jrvej.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/1200px-Altja_j%C3%B5gi_Lahemaal.jpg',
-    'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/w/o/WOPA160517_D056-resized.jpg?crop=864%2C0%2C1728%2C2304&wid=600&hei=800&scl=2.88',
-    'https://www.wbcsd.org/var/site/storage/images/overview/news-insights/wbcsd-insights/accelerating-business-along-the-road-to-a-nature-positive-future/225091-1-eng-GB/Accelerating-business-along-the-road-to-a-nature-positive-future_i1140.jpg',
-    'https://www.uwa.edu.au/news/-/media/project/uwa/uwa/newsroom/card-image/2023/value-of-nature-card.jpg',
-];
+/*
+Написати код, який за натиснення на кнопку створює і відображає червоний круг 80х80px
 
-const img = document.querySelector('.image');
-const [prevBtn, nextBtn] = document.querySelectorAll('.btn');
+За натиснення на червоний кружочок він має стати зеленим квадратиком
 
-const attrSrc = document.createAttribute('src');
-img.setAttributeNode(attrSrc);
+*/
+
+const btn = document.querySelector('#btn');
+
+const body = document.body;
+
+btn.addEventListener('click', function() {
+    const redCircle = document.createElement('div');
+    redCircle.style.backgroundColor = 'red';
+    redCircle.style.borderRadius = '50%';
+    redCircle.style.width = '80px';
+    redCircle.style.height = '80px';
+
+    redCircle.addEventListener('click', function (event) {
+        event.target.style.backgroundColor = 'green';
+        event.target.style.borderRadius = '0';
+    })
+
+    body.append(redCircle);
+})
 
 
-const slider = new Slider(images);
 
-
-function updateView() {
-    attrSrc.value = slider.currentSlide;
-}
-
-function createBtnHandler(direction) {
-    return function(){
-        slider.currentIndex = slider[direction];
-        updateView(); 
-    }
-}
-
-prevBtn.addEventListener('click', createBtnHandler('prev'))
-
-nextBtn.addEventListener('click', createBtnHandler('next'))
-
-updateView();
