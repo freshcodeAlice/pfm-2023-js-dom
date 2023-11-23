72c36578f8d90e170f9eb32afb2dd9ff10c9285b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-23 10:09:46 +0200

    side-menu task resolve

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-/*
-Написати код, який за натиснення на кнопку створює і відображає червоний круг 80х80px
+const menuContainer = document.querySelector('.menu-container');
+const menu = document.querySelector('.side-menu');
 
-За натиснення на червоний кружочок він має стати зеленим квадратиком
+// За натиснення на іконку гамбургера, змінювати стан бічного меню. Іконка має його закривати
 
-*/
-
-const btn = document.querySelector('#btn');
-
-const body = document.body;
-
-btn.addEventListener('click', function() {
-    const redCircle = document.createElement('div');
-    redCircle.className = 'red-circle';
-
-
-    redCircle.addEventListener('click', function (event) {
-//        event.target.className = 'green-square';
-
-    const cl = event.target.classList;
-    cl.toggle('green-square');
-    console.log(event.target.classList)
-    })
-
-    body.append(redCircle);
-})
-
-
-
+menuContainer.addEventListener('click', function(event) {
+    event.stopPropagation();
+    event.currentTarget.classList.toggle('change');
+    menu.classList.toggle('show');
+}, true);
