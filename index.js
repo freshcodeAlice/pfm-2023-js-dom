06c6217a491d61481ad9b8eea13ca7bedda4adc2
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-23 12:03:50 +0200

    input validation

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const form = document.querySelector('form');
+const inputFirstName = document.querySelector('input[name="firstName"]');
+
+
+inputFirstName.addEventListener('change', function({target}) {
+  
+    if(validateName(target.value)){
+        target.classList.remove('invalid');
+       target.classList.add('valid')
+    } else {
+        target.classList.add('invalid')
+    }
+})
+
+
+function validateName(value) {
+    return !(value.includes('@') || value.includes('!')) 
+}
 
 /*
 <form> за замовчуванням має специфічну поведінку - вона перезавантажує сторінку, намагаючись самостійно відправити дані туди, куди вказує атрибут action
@@ -8,7 +25,20 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function(event){
     // потрібно зупинити поведінку за замовчуванням!
     event.preventDefault(); // виклик методу зупиняє поведінку за-замовчуванням
-    const {firstName, lastName, email, pass, birthday, agreement} = event.target;
+    const form = event.target;
+    if (validateData(form.pass.value, form.passRepeat.value)) {
+        submitDataToServer(form);
+    } else {
+        createErrorMessage(form)
+    }
+})
+
+function validateData(inputValue1, inputValue2) {
+    return inputValue1 === inputValue2
+}
+
+
+function submitDataToServer({firstName, lastName, email, pass, passRepeat, birthday, agreement}){
     const userObj = {
         firstName: firstName.value,
         lastName: lastName.value,
@@ -17,13 +47,13 @@ form.addEventListener('submit', function(event){
         birthday: birthday.value,
         agree: agreement.checked
     }
-    console.log(userObj)
-})
+       console.log(userObj); // відправляємо дані на сервер
+}
 
 
-/*
-1. Додати мінімум стилів, прописати пояснення до кожного інпута, що це інформація
-
-2. За відправки форми вивести в консоль об'єкт зі всією введеною інформацією користувача
-
-*/
+function createErrorMessage(form) {
+        const errorMessage = document.createElement('p');
+        errorMessage.classList.add('error-text');
+        errorMessage.textContent = 'Password must be the same';
+        form.append(errorMessage);
+}
