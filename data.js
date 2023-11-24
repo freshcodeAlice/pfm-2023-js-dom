5ab06be2918cbecaf4a919e445dd38cd0f8934e8
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-24 12:04:41 +0200

    Hometask

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        username: 'Murzik',
+        userPic: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
+        age: '5',
+        id: 0
+    },
+    {
+        username: 'Barsik',
+        userPic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
+        age: '9',
+        id: 1
+    },
+    {
+        username: 'Tuzik',
+        userPic: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+        age: '9',
+        id: 2
+    },
+]
--- index.js
+++ index.js
@@ -1,55 +1,11 @@
 /*
-TodoList - Список справ
 
-+За введення в форму та натиснення на кнопку під формою має з'явитись новий пункт списку
+Таска:
+при відкритті сторінки
+відобразити карточку кожного кота
 
-ТЗ:
-+1. Не створювати новий пункт списку, якщо в інпуті пусто
-+2. Кожна лі-шка має кнопку самоліквідації. За натиснення цей пункт списку видаляється.
+Порада: створити спочатку верстку 1 картки, прописати стилі, потім закоментувати верстку і створювати всі ті самі елементи динамічно
 
-+3. Всі пункти списку мають зберігатись в певній колекції.
+ */
 
-
-*/
-
-
-const form = document.querySelector('.todo-form');
-const list = document.querySelector('.todo-list');
-
-let todoArray = [];
-let counter = 0;
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const {todo: {value: todoText}} = event.target;
-    if (todoText){
-        todoArray.push({
-            text: todoText,
-            id: counter++
-        });
-    }
-    updateView(todoArray);
-});
-
-
-
-function updateView(todoArray) {
-    const liArray = todoArray.map(todoObj => {
-        const li = document.createElement('li');
-        li.append(todoObj.text);
-        li.dataset.id = todoObj.id;
-        const button = document.createElement('button');
-        button.textContent = 'X';
-        button.addEventListener('click', deleteButtonHandler)
-        li.append(button);
-        return li;
-    });
-    list.replaceChildren(...liArray);
-}
-
-
-function deleteButtonHandler(event) {
-  const parentLi = event.target.parentElement;
-  todoArray = todoArray.filter(elem => elem.id !== Number(parentLi.dataset.id));
-  updateView(todoArray);
-}
+console.log(data);
