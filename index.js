3fd0485d73dad0de46e38021bbe5de6c6f7d1550
freshcodeAlice <freshjs.ofte@gmail.com>
2023-11-28 09:37:40 +0200

    async

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-function foo(){
-    setTimeout(() => {
-        foo();
-    }, 0)
+setTimeout(() => {
+    console.log('DONE AFTER ALL SYNC WORK');
+    anotherAfterWork();
+}, 0)
+
+
+function syn1(fn, fn1) {
+    console.log('first sync work');
+    // syn2()
+    fn(fn1);
 }
+
+function syn2(fn1) {
+    console.log('second sync work');
+    // syn3()
+    fn1();
+}
+
+function syn3() {
+    console.log('third sync work');
+}
+
+syn1(syn2, syn3);
+
+
+
+function anotherAfterWork() {
+    console.log('after async');
+}
+
+
+setTimeout(function() {   /* micro-callback-hell */
+    console.log(1);
+    setTimeout(()=> {
+        console.log(2);
+        setTimeout(()=>{
+            console.log(3);
+            setTimeout(()=>{
+                console.log(4);
+            },0)
+        },0)
+    },0)
+},0)
+
+/*
+Promise - особливий об'єкт, який використовується для асинхронної роботи
+
+Має один з трьох станів:
+    - очікування (pending) - в цьому стані він "народжується"
+З нього переходить в 
+або fullfiled (успішно виконаний) - resolved
+або rejected (відхилений, помилка)
+
+Проміс - одноразовий, він змінює стан тільки один раз
+*/
